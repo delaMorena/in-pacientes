aa6a1eb79e565834ea0bfc56ade4029459e4c24b
delaMorena <martadmdo@gmail.com>
2020-12-04 20:23:12 +0000

    clase 04/12/2020

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,22 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# ''' empiezo a definir los endpoint '''
+
+# @api.route('/users', method=['GET'])
+# def handle_list_all_users():
+#     ''' Return List of user'''
+#     return 'List all users'
+
+# @api.route('/users/<int:id>', method= ['GET'])
+# def handle_get_user(id):
+#     ''' Return one single user '''
+#     return 'Get #{} user.'.format(id)
+
+# @api.route('/users', method= ['POST'])
+# def handle_create_user():
+#     ''' Create user '''
+#     payload= request.get_json()
+#     print(payload)
+#     return 'User created'
