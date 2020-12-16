05863b14f21a0364133c4c25fa812f2675f9adad
delaMorena <martadmdo@gmail.com>
2020-12-16 13:45:25 +0000

    empiezo con las peticiones de  relationships

--- migrations/versions/dc431f4f39f0_.py
+++ migrations/versions/dc431f4f39f0_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 149ec6ce369c
+Revision ID: dc431f4f39f0
 Revises: 
-Create Date: 2020-12-16 11:38:11.302169
+Create Date: 2020-12-16 13:06:54.094302
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '149ec6ce369c'
+revision = 'dc431f4f39f0'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/api/routes.py
+++ src/api/routes.py
@@ -143,7 +143,7 @@ def handle_update_disease(id):
 
     payload = request.get_json()
 
-    # Añadir un if para cada clave del payload
+    # Añadido un if para cada clave del payload
     if "title" in payload:
         disease.title = payload['title']
 
@@ -230,10 +230,15 @@ def handle_update_post(id):
 
     payload = request.get_json()
 
-    # Añadir un if para cada clave del payload
-    post.text = payload['text']
-    post.publisher_id = payload['publisher_id']
-    post.disease_id = payload['disease_id']
+    # Añadido un if para cada clave del payload
+    if "text" in payload:
+        post.text = payload['text']
+    
+    if "publisher_id" in payload:
+        post.publisher_id = payload['publisher_id']
+
+    if "disease_id" in payload:
+        post.disease_id = payload['disease_id']
 
     db.session.add(post)
     db.session.commit()
@@ -314,7 +319,7 @@ def handle_create_follow():
     print(payload)
     return "follow created"
 
-
+# Tiene sentido editar un follow? como se marca y se desmarca? es un borrado o un put?:
 # @api.route("/follows/<int:user_id>/<int:disease_id>", methods= ["PUT"])
 # def handle_update_follow(user_id, disease_id):
 #     """ Update existing follow """
@@ -336,17 +341,48 @@ def handle_list_all_roles():
     """ Return List of follows"""
     return "List all follows"
 
+
+# def handle_list_all_relationships():
+#     relationships = []
+
+#     for relationship in Relationships.query.all():
+#         relationship.append(user.serialize())
+#     return jsonify(relationships), 200
+
 @api.route("/users/<int:user_id>/relationships", methods=["GET"])
 def handle_get_user_roles(user_id):
     """ Return the amount of roles of an user"""
     return "Get roles of #{} user.".format(user_id)
 
 
+# def handle_list_roles_from_user(id):
+#     user = Users.query.get(id)
+#     relationships = []
+
+#     if not user:
+#         return "User not found", 404
+
+#     for relationship in user.relationship:
+#         relationships.append(relationship.serialize())
+        
+#     return jsonify(relationships), 200
+
+
 @api.route("/diseases/<int:disease_id>/relationships", methods=["GET"])
 def handle_get_disease_roles(disease_id):
     """ Return the amount of roles of a disease"""
     return "Get roles of #{} disease.".format(disease_id)
+# def handle_list_relationships_from_disease(id):
+#     disease = Diseases.query.get(id)
+#     relationships = []
 
+#     if not disease:
+#         return "Disease not found", 404
+
+#     for relationship in disease.relationships:
+#         relationships.append(relationship.serialize())
+        
+#     return jsonify(relationships), 200
 
 @api.route("/diseases/<int:disease_id>/relationships", methods=["POST"])
 def handle_create_role_for_disease():
