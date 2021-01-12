cf163d3980293ac88b114372db0d231dd8fe3785
delaMorena <martadmdo@gmail.com>
2021-01-12 12:12:54 +0000

    terminada la primera maquetación de las views y components

--- src/front/js/views/profile.js
+++ src/front/js/views/profile.js
@@ -1,6 +1,8 @@
 // PAGINA DE USUARIO CON LOS POST QUE HAS PUBLICADO USANDO LAS VISTAS DE cardFeed.js
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Header } from "../component/header.js";
+import { CardFeed } from "../component/card-feed.js";
 
 export const Profile = () => {
 	const { store, actions } = useContext(Context);
@@ -8,6 +10,19 @@ export const Profile = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Perfil de usuario</h1>
+			<Header />
+			<div role="tabpanel" aria-labelledby="pills-grid">
+				<div className="container my-3">
+					<div className="row no-gutters">
+						<CardFeed />
+						<CardFeed />
+						<CardFeed />
+						<CardFeed />
+						<CardFeed />
+						<CardFeed />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
