f3ffa40a52333c438c105ab7d0e3be4e3a94a5af
delaMorena <martadmdo@gmail.com>
2021-01-13 12:00:47 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import "../../styles/footer.scss";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer row">
+		<div className="col-7 left-footer">
+			<p>
+				Made with <i className="fa fa-heart text-danger" /> by Alex {"&"} Marta 2021
+			</p>
+		</div>
+		<div className="col-5 right-footer">
+			<p>Aviso Legal-Política de Privacidad-Política de Cookies</p>
+		</div>
 	</footer>
 );
