49aa34698e724c004205888ca0cdc1e3d514317f
delaMorena <martadmdo@gmail.com>
2021-01-13 13:26:12 +0000

    estilos y pruebas

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,13 +3,9 @@ import "../../styles/footer.scss";
 
 export const Footer = () => (
 	<footer className="footer row">
-		<div className="col-7 left-footer">
-			<p>
-				Made with <i className="fa fa-heart text-danger" /> by Alex {"&"} Marta 2021
-			</p>
+		<div className="col-4">
+			Made with <i className="fa fa-heart text-danger" /> by Alex {"&"} Marta 2021
 		</div>
-		<div className="col-5 right-footer">
-			<p>Aviso Legal-Política de Privacidad-Política de Cookies</p>
-		</div>
+		<div className="col-8">Aviso Legal-Política de Privacidad-Política de Cookies</div>
 	</footer>
 );
