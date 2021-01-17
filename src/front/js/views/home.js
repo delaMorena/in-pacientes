bd5df8dd19a751b6e027349cb24cfc78fa7a9d21
delaMorena <martadmdo@gmail.com>
2021-01-17 22:41:55 +0000

    algunos estilos añadidos

--- src/front/js/views/home.js
+++ src/front/js/views/home.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import { Link } from "react-router-dom";
-// import "../../styles/home.scss";
+import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -11,21 +11,21 @@ export const Home = () => {
 		<div className="fluid-container">
 			<div className="jumbotron text-center">
 				<h1 className="display-4">In-Pacientes</h1>
-				<p className="lead">Algun slogan</p>
+				<p className="lead">LOOKING FOR YOU</p>
 
 				<p>
-					It uses utility classNamees for typography and spacing to space content out within the larger
-					container.
+					We want to be your tribe. We are looking for you. Join us in this journey that will improve your
+					life. FOR SURE
 				</p>
 				<div className="row d-flex justify-content-around">
 					<div className="col-4">
 						<Link to="/login">
-							<button className="btn btn-primary btn-lg">Iniciar Sesión</button>
+							<button className="btn btn-lg">Iniciar Sesión</button>
 						</Link>
 					</div>
 					<div className="col-4">
 						<Link to="/signup">
-							<button className="btn btn-primary btn-lg">Registrarme</button>
+							<button className="btn btn-lg">Registrarme</button>
 						</Link>
 					</div>
 				</div>
