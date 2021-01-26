68835d07802f6d8dae4037f357119a0b5cc9210e
ARedondoVilla <alexredondovilla@gmail.com>
2021-01-26 16:13:24 +0000

    footer arreglado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 import "../../styles/footer.scss";
 
 export const Footer = () => (
-	<footer id="styleFooter" className="row d-flex justify-content-center ml-5 py-3">
-		<div className="col-9">
+	// <footer id="styleFooter" className="row d-flex justify-content-center ml-5 py-3">
+	// 	<div className="col-9">
+	// 		Made with <i className="fa fa-heart text-danger" /> by Alex {"&"} Marta 2021
+	// 	</div>
+	// 	<div className="col-3">
+	// 		<Link to="/about-us">
+	// 			<p>About Us</p>
+	// 		</Link>
+	// 	</div>
+	// </footer>
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
 			Made with <i className="fa fa-heart text-danger" /> by Alex {"&"} Marta 2021
-		</div>
-		<div className="col-3">
-			<Link to="/about-us">
-				<p>About Us</p>
-			</Link>
-		</div>
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
 	</footer>
 );
